223fd8bd4b10bd9e789e149323e34be9bf00ff71
thecontinium <thecontinium@outlook.com>
2019-01-22 17:57:11 +0800

    initial

--- Formula/setup-xdg-env.rb
+++ Formula/setup-xdg-env.rb
@@ -1,7 +1,7 @@
 class SetupXdgEnv< Formula
   homepage 'https://github.com/thecontinium/homebrew-setup-xdg-env/'
   url 'https://github.com/thecontinium/homebrew-setup-xdg-env.git'
-  version '0.0.3'
+  version '0.0.4'
 
   def install
 	    pefix.install Dir["config/*.plist"]
