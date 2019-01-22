8159b999732c2ce87613b400b4e8f843ef60aca5
thecontinium <thecontinium@outlook.com>
2019-01-22 16:57:02 +0800

    initial

--- Formula/setup-xdg-env.rb
+++ Formula/setup-xdg-env.rb
@@ -1,14 +1,32 @@
 class SetupXdgEnv< Formula
   homepage 'https://github.com/thecontinium/homebrew-setup-xdg-zsh-env/'
   url 'https://github.com/thecontinium/homebrew-setup-xdg-zsh-env.git'
-  version '0.0.1'
+  version '0.0.2'
 
   skip_clean 'bin'
 
-  def install
-    prefix.install Dir["config/*.plist"]
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>homebrew.mxcl.setup-xdg-env</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>sh</string>
+            <string>-c</string>
+            <string>
+                launchctl setenv XDG_CONFIG_HOME $HOME/.config
+                launchctl setenv XDG_CACHE_HOME $HOME/.cache
+                launchctl setenv XDG_DATA_HOME $HOME/.local/share
+                launchctl setenv XDG_RUNTIME_DIR /tmp/run</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+    EOS
   end
-
-  plist_options :startup  => true
-
 end
+
